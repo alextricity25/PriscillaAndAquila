d6e7d563db13fba3cc1f801c4609faf758967321
alextricity25 <miguel.can2@gmail.com>
2019-06-10 18:18:05 -0700

    Verses can now be listed without book name or chap
    Code is added to implement the ability to list verses after a book name or
    chapter. For example, John 3:16, 12-13, 12:24.
    Verses that are listed without a book name will inherit the most recently
    mentioned book name. The same principle is used to deduce chapter numbers
    for verses with no chapter reference.
    
    Implements: https://github.com/alextricity25/PriscillaAndAquila/issues/1

--- python/priscillaandaquila/main.py
+++ python/priscillaandaquila/main.py
@@ -27,17 +27,27 @@ def main():
     else:
         raise Exception("Choose an argument!")
     for verse_ref in verse_refs:
+        # Verses in the same book can be subsequently listed without the
+        # the book name. This is handled seperately by _expand_verse_range
+        # by considering first the reference with the book name, then any
+        # verses that may be listed.
+        _ = verse_ref.split(',')
+        verse_ref = _[0]
+        listed_verses = _[1:]
         # Expand the verse reference, so that users can also
         # provide book abbrivations.
         extrapolated_verse_ref = bible.extrapolate_abbriv(verse_ref.strip(),
                                                           raise_exp = False)
         # Splitting verse to book, chapter, verse(s)
-        reg = re.compile('(([0-9]*) *[\D]+) *([0-9]*):([0-9]*-*[0-9]*)')
-        _verse_refs = _expand_verse_range(reg, extrapolated_verse_ref)
+        reg = re.compile('(([0-9] )*[a-zA-Z]*) *([0-9]*:)*([0-9]*-*[0-9]*)')
+        _verse_refs = _expand_verse_range(reg,
+                                          extrapolated_verse_ref,
+                                          listed_verses)
+
         for _verse_ref in _verse_refs:
             m = reg.match(_verse_ref)
             verse = bible.verse_lookup(m.group(1).strip(),
-                                       m.group(3).strip(),
+                                       m.group(3).strip().replace(":",""),
                                        m.group(4).strip())
             # Strip off identifier returned by "verse_lookup"
             verse = re.sub('[0-9]*\.', '', verse).strip()
@@ -45,17 +55,17 @@ def main():
             if parsed_args.references_first:
                 print "{} {}:{} - {}".format(
                     m.group(1).strip(),
-                    m.group(3).strip(),
+                    m.group(3).strip().replace(":",""),
                     m.group(4).strip(),
                     verse)
             else:
                 print "{} - {} {}:{}".format(
                     verse,
                     m.group(1).strip(),
-                    m.group(3).strip(),
+                    m.group(3).strip().replace(":", ""),
                     m.group(4).strip())
 
-def _expand_verse_range(reg, verse_ref):
+def _expand_verse_range(reg, verse_ref, listed_verses):
     """
     Takes in a Reg  object with very specific groupings. The
     groupings must follow these specifications:
@@ -70,22 +80,42 @@ def _expand_verse_range(reg, verse_ref):
     verses in that range, inclusively.
 
     The second argument is the verse reference to be expanded.
+
+    The third argument is a list of verses, without book reference, whose
+    book name will be the same as the one used for ``verse_ref``
     """
-    verses = []
+    verses_expanded = []
+    # Get the book name and chapter of verse_ref, might be used
+    # later to deduce the full reference in listed_verses
     m = reg.match(verse_ref)
-    if "-" in m.group(4):
-        verse_ends = m.group(4).split('-')
-        for i in xrange(int(verse_ends[0]), int(verse_ends[1]) + 1):
-            verses.append("{} {}:{}".format(
-                m.group(1).strip(),
-                m.group(3).strip(),
-                str(i)))
-        return verses
-    else:
-        return ["{} {}:{}".format(
-            m.group(1).strip(),
-            m.group(3).strip(),
-            m.group(4).strip())]
+    book_name = m.group(1).strip()
+    chapter = m.group(3).strip().replace(":","")
+
+    for verses in [verse_ref] + listed_verses:
+        m = reg.match(verses.strip())
+
+
+        # If ":" exist in the string, that indicates a new chapter is being
+        # referenced.
+        if ":" in verses:
+            chapter = m.group(3).replace(":","")
+
+        # If "-" exist in the string, that indicates a range of verses that
+        # needs to be expanded.
+        if "-" in m.group(4):
+            verse_ends = m.group(4).split('-')
+            for i in xrange(int(verse_ends[0]), int(verse_ends[1]) + 1):
+                verses_expanded.append("{} {}:{}".format(
+                    book_name,
+                    chapter,
+                    str(i)))
+        else:
+            verses_expanded.append("{} {}:{}".format(
+                book_name,
+                chapter,
+                m.group(4).strip()))
+
+    return verses_expanded
 
 
 if __name__ == '__main__':
